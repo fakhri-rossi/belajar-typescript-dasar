0defd003a02f2573226f5a5af858b4080e77a80d
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-02 09:02:48 +0700

    Babel and TS

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,10 +1,16 @@
 describe("Data Type", function () {
   it("should must declare", function () {
-    const name: string = "Fakhri Rossi";
-    const balance: number = 1000000000;
-    const isVip: boolean = true;
+    let name: string = "Fakhri Rossi";
+    let balance: number = 1000000000;
+    let isVip: boolean = true;
+
     console.info(name);
     console.info(balance);
     console.info(isVip);
+
+    // to run tsc continuously, run npx tsc --watch
+    // name = 300;
+    // balance = "rossi";
+    // isVip = "wkwk";
   });
 });
