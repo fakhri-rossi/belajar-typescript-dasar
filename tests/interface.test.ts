bfd56f3bc14063fa0fc6ffad6e6e333ddd441d60
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-02 14:56:04 +0700

    Function Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,7 +1,7 @@
 import { Seller } from "../src/seller";
 
 describe("Interface", function () {
-  it("should support i typescript", function () {
+  it("should support interface typescript", function () {
     const seller: Seller = {
       id: 1,
       name: "Toko Rossi",
@@ -13,5 +13,18 @@ describe("Interface", function () {
     // seller.nib = '24252626'; //bakal error
 
     console.info(seller);
+  });
+
+  it("should support function interface", function () {
+    interface AddFunction {
+      (value1: number, value2: number): number;
+    }
+
+    // const add: AddFunction = (value1: number, value2: number): number => { // boleh secara eksplisit, boleh engga
+    const add: AddFunction = (value1, value2) => {
+      return value1 + value2;
+    };
+
+    expect(add(2, 2)).toBe(4);
   });
 });
