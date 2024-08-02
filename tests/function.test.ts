9a25f2e86705221011cab1bd39cd6b1ac1f76f96
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-02 16:06:09 +0700

    Function Parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -12,4 +12,35 @@ describe("Function", function () {
 
     printHello("Rossi");
   });
+
+  it("should support default value", function () {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello, ${name}`;
+    }
+    expect(sayHello()).toBe("Hello, Guest");
+    expect(sayHello("Rossi")).toBe("Hello, Rossi");
+  });
+
+  it("should support rest parameter", function () {
+    function sum(...values: number[]): number {
+      let total = 0;
+      for (const value of values) {
+        total += value;
+      }
+      return total;
+    }
+
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it("should support optional parameter", function () {
+    function sayHello(firstName: string, lastName?: string): string {
+      return lastName
+        ? `Hello, ${firstName} ${lastName}`
+        : `Hello, ${firstName}`;
+    }
+
+    expect(sayHello("Rossi")).toBe("Hello, Rossi");
+    expect(sayHello("Rossi", "Fakhri")).toBe("Hello, Rossi Fakhri");
+  });
 });
