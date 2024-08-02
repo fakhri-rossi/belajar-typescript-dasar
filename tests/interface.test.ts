91fa41bca10f0094fd503252a22120a2ae6a4a7c
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-02 14:46:07 +0700

    Readonly Properties

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -4,8 +4,13 @@ describe("Interface", function () {
   it("should support i typescript", function () {
     const seller: Seller = {
       id: 1,
-      name: "Rossi",
+      name: "Toko Rossi",
+      nib: "172228",
+      npwp: "17262399",
     };
+
+    seller.name = "Toko Andi";
+    // seller.nib = '24252626'; //bakal error
 
     console.info(seller);
   });
